8564f180f664d9b5f9a8af391aa8604a329097b3
qiqihannah <784960656@qq.com>
2021-08-16 16:36:01 +0800

    Add files via upload

---
+++ src/baiduMap.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import net.sf.json.JSONObject;
+
+
+public class baiduMap {
+    public static String doGet(String httpurl) {
+        HttpURLConnection connection = null;
+        InputStream is = null;
+        BufferedReader br = null;
+        String result = null;// 返回结果字符串
+        try {
+            // 创建远程url连接对象
+            URL url = new URL(httpurl);
+            // 通过远程url连接对象打开一个连接，强转成httpURLConnection类
+            connection = (HttpURLConnection) url.openConnection();
+            // 设置连接方式：get
+            connection.setRequestMethod("GET");
+            // 设置连接主机服务器的超时时间：15000毫秒
+            connection.setConnectTimeout(15000);
+            // 设置读取远程返回的数据时间：60000毫秒
+            connection.setReadTimeout(60000);
+            // 发送请求
+            connection.connect();
+            // 通过connection连接，获取输入流
+            if (connection.getResponseCode() == 200) {
+                is = connection.getInputStream();
+                // 封装输入流is，并指定字符集
+                br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                // 存放数据
+                StringBuffer sbf = new StringBuffer();
+                String temp = null;
+                while ((temp = br.readLine()) != null) {
+                    sbf.append(temp);
+                    sbf.append("\r\n");
+                }
+                result = sbf.toString();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭资源
+            if (null != br) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            connection.disconnect();// 关闭远程连接
+        }
+
+        return result;
+    }
+    public static String get_info(String id) {
+        String url = "https://api.map.baidu.com/weather/v1/?district_id=" + id + "&data_type=all&ak=agUT13GLF03Lg1uzpLDFpVqT4t2Sr0iO";
+        String json = doGet(url);
+        String weather = null;
+        try {
+            JSONObject jsonObject = JSONObject.fromObject(json);
+            JSONObject jsonResult = jsonObject.getJSONObject("result");
+            JSONObject jsonNow = jsonResult.getJSONObject("now");
+            weather = jsonNow.getString("temp");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return weather;
+    }
+    public static void main(String[] args) {
+        System.out.println(get_info("320581"));
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        String t1 = baiduMap.get_info("320581");
+        String t2 = weather.getWeather("2008");
+        System.out.println(t1);
+        System.out.println(t2);
+    }
+}
